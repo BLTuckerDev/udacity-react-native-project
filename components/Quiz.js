754fd020ffe162285fce7bba191375dab3308593
Brett <bretttucker@gmail.com>
2018-07-21 11:22:50 -0700

    feat: Implement end of quiz screen
    This fulfills a rubric requirement for the Quiz component

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {appBlue} from "../utils/colors";
 
 
 const styles = StyleSheet.create({
@@ -32,12 +33,14 @@ const styles = StyleSheet.create({
         marginTop: 20,
         backgroundColor: "#0F0",
         padding: 10,
+        height: 48,
         borderRadius: 5
     },
     incorrectButton: {
         marginTop: 20,
         backgroundColor: "#F00",
         padding: 10,
+        height: 48,
         borderRadius: 5
 
     },
@@ -50,6 +53,38 @@ const styles = StyleSheet.create({
         color: "#FFF",
         fontSize: 18,
 
+    },
+    scoreText: {
+        marginTop: 20,
+        fontSize: 24,
+        color: "#000"
+    },
+    restartQuizButton: {
+        backgroundColor: appBlue,
+        padding: 20,
+        height: 48,
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginTop: 40
+    },
+    backToDeckButton: {
+        padding: 20,
+        height: 48,
+        justifyContent: 'center',
+        borderColor: "#000",
+        borderWidth: 1,
+        borderRadius: 5,
+        marginTop: 20,
+        marginBottom: 20
+
+    },
+    restartQuizButtonText: {
+        color: "#FFF",
+        fontSize: 18,
+    },
+    backToDeckButtonText: {
+        fontSize: 18,
+        color: '#000'
     }
 });
 
@@ -63,20 +98,48 @@ class Quiz extends Component {
 
     state = {
         currentCard: 1,
-        showQuestion: true
+        showQuestion: true,
+        correctAnswers: 0
     };
 
     nextCard = () => {
-        const nextCard = this.state.currentCard === this.props.questions.length ? 1 : this.state.currentCard + 1;
+        const nextCard = this.state.currentCard + 1;
         this.setState({currentCard: nextCard})
     };
 
+    restartQuiz = () => {
+        this.setState({currentCard: 1, showQuestion: true, correctAnswers: 0})
+    };
+
+    backToDeck = () => {
+        this.props.navigation.pop();
+    };
 
     render() {
         const {questions} = this.props;
         const totalCards = questions.length;
+
+        if(this.state.currentCard === questions.length + 1){
+
+            const percentCorrect = ((this.state.correctAnswers / questions.length) * 100).toFixed(0);
+
+            return (
+                <View style={styles.center}>
+                    <Text style={styles.scoreText}>Good Job! You got {percentCorrect}% Correct</Text>
+                    <TouchableOpacity style={styles.restartQuizButton} onPress={() => {this.restartQuiz()}}>
+                        <Text style={styles.restartQuizButtonText}>Restart Quiz</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.backToDeckButton} onPress={() => this.backToDeck()}>
+                        <Text style={styles.backToDeckButtonText}>Back To Deck</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+
+
         const currentCard = questions[this.state.currentCard - 1];
         const showQuestion = this.state.showQuestion;
+
 
         return (
             <View style={styles.center}>
@@ -84,7 +147,9 @@ class Quiz extends Component {
                 <Text style={styles.cardText}>{showQuestion ? currentCard.question : currentCard.answer}</Text>
                 <Text style={styles.cardSubtext}
                       onPress={() => this.setState({showQuestion: !showQuestion})}>{showQuestion ? "Answer" : "Question"}</Text>
-                <TouchableOpacity style={styles.correctButton} onPress={() => this.nextCard()}>
+                <TouchableOpacity style={styles.correctButton} onPress={() => {
+                    this.setState({correctAnswers: this.state.correctAnswers+1});
+                    this.nextCard()}}>
                     <Text style={styles.correctButtonText}>Correct</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.incorrectButton} onPress={() => this.nextCard()}>
